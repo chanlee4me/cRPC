7cd6c85869adc079bf5716ef881525a548edf637
chanlee4me <cli1004@163.com>
2025-04-18 21:54:46 +0800

    feature：V1-新增公共错误码类

---
+++ src/main/java/com/chanlee/crpc/v1/common/convention/BaseErrorCode.java
@@ -0,0 +1,23 @@
+package com.chanlee.crpc.v1.common.convention;
+
+public enum BaseErrorCode implements ErrorCode {
+    SERVER_ERROR("A000001", "服务端内部错误");
+
+    private final String code;
+
+    private final String message;
+
+    BaseErrorCode(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+    @Override
+    public String code() {
+        return code;
+    }
+
+    @Override
+    public String message() {
+        return message;
+    }
+}
---
+++ src/main/java/com/chanlee/crpc/v1/common/convention/ErrorCode.java
@@ -0,0 +1,13 @@
+package com.chanlee.crpc.v1.common.convention;
+
+public interface ErrorCode {
+    /**
+     * 错误码
+     */
+    String code();
+
+    /**
+     * 错误信息
+     */
+    String message();
+}
