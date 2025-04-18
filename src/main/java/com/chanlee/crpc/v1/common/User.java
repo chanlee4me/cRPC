32448655eb571017474fa15e44f9a509f3046f25
chanlee4me <cli1004@163.com>
2025-04-18 21:56:11 +0800

    feature：V1-用户类

---
+++ src/main/java/com/chanlee/crpc/v1/common/User.java
@@ -0,0 +1,31 @@
+package com.chanlee.crpc.v1.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+/**
+ * 用户类
+ */
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User implements Serializable {
+    /**
+     * 用户id
+     */
+    Integer id;
+
+    /**
+     * 用户真实姓名
+     */
+    String realName;
+
+    /**
+     * 用户年龄
+     */
+    Integer age;
+}
