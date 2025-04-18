cbe0833238ff3942f4ab4b4f6c097e194c15616f
chanlee4me <cli1004@163.com>
2025-04-18 21:55:35 +0800

    feature：V1-新增封装的请求对象体

---
+++ src/main/java/com/chanlee/crpc/v1/common/RpcRequest.java
@@ -0,0 +1,33 @@
+package com.chanlee.crpc.v1.common;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * 请求对象体
+ */
+@Builder
+@Data
+public class RpcRequest implements Serializable {
+    /**
+     * 接口名
+     */
+    private String interfaceName;
+
+    /**
+     * 方法名
+     */
+    private String method;
+
+    /**
+     * 参数
+     */
+    private Object[] params;
+
+    /**
+     * 参数类型
+     */
+    private Class<?>[] paramsTypes;
+}
