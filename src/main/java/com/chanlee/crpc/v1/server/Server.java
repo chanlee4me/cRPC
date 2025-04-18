c43c38e859fe3d32855fee3a237cb7db58551bc5
chanlee4me <cli1004@163.com>
2025-04-18 21:55:53 +0800

    feature：V1-更新服务端代码

---
+++ src/main/java/com/chanlee/crpc/v1/server/Server.java
@@ -0,0 +1,51 @@
+package com.chanlee.crpc.v1.server;
+
+import com.chanlee.crpc.v1.common.RpcRequest;
+import com.chanlee.crpc.v1.common.RpcResponse;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.ServerSocket;
+import java.net.Socket;
+/**
+ * 服务端代码
+ */
+public class Server {
+    private static final int SERVER_PORT = 8003;
+
+        public static void main(String[] args) {
+
+            UserServiceImpl userService = new UserServiceImpl();
+
+            try (ServerSocket serverSocket = new ServerSocket(SERVER_PORT)){
+                System.out.println("服务器已启动...");
+                while(true){
+                    Socket socket = serverSocket.accept();
+                    //接收到连接请求后，启动一个新线程去处理任务
+                    new Thread(() -> {
+                        try {
+                            // 获取输入、输出流
+                            ObjectOutputStream objectOutput = new ObjectOutputStream(socket.getOutputStream());
+                            ObjectInputStream objectInput = new ObjectInputStream(socket.getInputStream());
+                            //读取接收到的请求
+                            RpcRequest request = (RpcRequest) objectInput.readObject();
+                            //利用反射调用对应方法
+                            Method method = userService.getClass().getMethod(request.getMethod(), request.getParamsTypes());
+                            Object invoke = method.invoke(userService, request.getParams());
+                            //将调用结果进行封装
+                            objectOutput.writeObject(RpcResponse.success(invoke));
+                            //及时传递消息
+                            objectOutput.flush();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }).start();
+                }
+            } catch (IOException e) {
+                System.out.println("服务器启动失败...");
+            }
+        }
+}
