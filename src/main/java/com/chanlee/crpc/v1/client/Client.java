3715da2c5d48fe654acf7420fc4650098e1e1539
chanlee4me <cli1004@163.com>
2025-04-18 21:55:07 +0800

    feature：V1-更新客户端代码

---
+++ src/main/java/com/chanlee/crpc/v1/client/Client.java
@@ -0,0 +1,33 @@
+package com.chanlee.crpc.v1.client;
+
+import com.chanlee.crpc.v1.common.User;
+import com.chanlee.crpc.v1.service.UserService;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+/**
+ * 客户端代码
+ */
+public class Client{
+
+    public static void main(String[] args){
+        ClientProxy clientProxy = new ClientProxy("127.0.0.1", 8003);
+        UserService proxy = clientProxy.getProxy(UserService.class);
+
+        //调用方法 1
+        User user = proxy.getUserById(1);
+        System.out.println("对应的user为：" + user);
+        //调用方法 2
+        User codingBoy = User.builder()
+                .age(25)
+                .id(32)
+                .realName("coding boy")
+                .build();
+        Integer i = proxy.insertUser(codingBoy);
+        System.out.println("向服务端插入的 user的Id为：" + i);
+    }
+
+}
