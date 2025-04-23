06742daa989cfa56dcea33d98b7dcfa2b5cf6e3d
chanlee4me <cli1004@163.com>
2025-04-23 10:36:12 +0800

    feature：V2-客户端代理类

---
+++ src/main/java/com/chanlee/crpc/v2/client/ClientProxy.java
@@ -0,0 +1,50 @@
+package com.chanlee.crpc.v2.client;
+
+import com.chanlee.crpc.v2.dto.RpcRequestDTO;
+import com.chanlee.crpc.v2.dto.RpcResponseDTO;
+import lombok.AllArgsConstructor;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+import static com.chanlee.crpc.v2.client.IOClient.sendRequest;
+
+/**
+ * 客户端代理类
+ */
+@AllArgsConstructor
+public class ClientProxy implements InvocationHandler {
+    /**
+     * 服务端 IP
+     */
+    private String host;
+
+    /**
+     * 服务端端口号
+     */
+    private int port;
+
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        if(method.getDeclaringClass() == Object.class){}
+        //构建request请求
+        RpcRequestDTO request = RpcRequestDTO.builder()
+                .interfaceName(method.getDeclaringClass().getName())
+                .method(method.getName())
+                .paramsTypes(method.getParameterTypes())
+                .params(args)
+                .build();
+        //发送请求并获取响应
+        RpcResponseDTO<Object> response = sendRequest(host, port, request);
+        //返回结果数据
+        return response.getData();
+    }
+    public <T> T getProxy(Class<T> tClass){
+        Object o = Proxy.newProxyInstance(
+                tClass.getClassLoader(),
+                new Class[]{tClass},
+                this
+        );
+        return (T)o;
+    }
+}
